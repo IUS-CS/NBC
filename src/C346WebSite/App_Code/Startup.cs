7bc0022087eac91706ac09487589a10d4506cbb0
Cody <martincody423@gmail.com>
2017-09-27 15:02:55 -0400

    added source code

---
+++ src/C346WebSite/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(C346WebSite.Startup))]
+namespace C346WebSite
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
