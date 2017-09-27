1fe9f3571ca69009a2121c8adfb0e6a9f980a602
Brad Adams <adamsbc.ius.edu>
2017-09-27 15:01:34 -0400

    deleting src

--- src/Scripts/_references.js
+++
@@ -1,5 +0,0 @@
-﻿/// <autosync enabled="true" />
-/// <reference path="bootstrap.min.js" />
-/// <reference path="jquery-1.10.2.min.js" />
-/// <reference path="modernizr-2.6.2.js" />
-/// <reference path="respond.min.js" />
